21eff2a091d7be719ef475e66426bcbe85472bd0
christian igay <christian.igay@gmail.com>
2025-08-04 13:57:14 +0800

    auth routes

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin/auth')->controller(AuthController::class)->group(function () {
-    Route::post('/register', 'register')->name('admin.auth.register');
-    Route::post('/login', 'login')->name('admin.auth.login');
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register')->name('auth.register');
+    Route::post('/login', 'login')->name('auth.login');
     
     Route::middleware('auth:api')->group(function () {
-        Route::post('/logout', 'logout')->name('admin.auth.logout');
-        Route::get('/user', 'user')->name('admin.auth.user');
+        Route::post('/logout', 'logout')->name('auth.logout');
+        Route::get('/user', 'user')->name('auth.user');
     });
 });
