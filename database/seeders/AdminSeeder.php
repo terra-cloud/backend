f6794a07e6000289366f4d016ab39dbcf4033457
christian igay <christian.igay@gmail.com>
2025-08-06 20:26:10 +0800

    admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'superadmin@terra.com',
+            'password' => bcrypt('terra123123123'),
+            'status' => 1
+        ]);
+    }
+}
