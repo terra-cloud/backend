9482ab208133aaf5dcabc19a01a6149094661cea
Christian <christian.igay@gmail.com>
2025-09-08 19:46:29 +0800

    remove account

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::post('/register', 'register')->name('auth.register');
     Route::post('/login', 'login')->name('auth.login');
-    Route::post('/remove-account', 'removeAccount')->name('user.remove_account');
+    Route::delete('/remove-account', 'removeAccount')->name('user.remove_account');
     
     Route::middleware('auth:api')->group(function () {
         Route::post('/logout', 'logout')->name('auth.logout');
